5dcd9a08a9cb8f3d9a50e0d8c849169609dfcc41
ayoub <ayoub.barich@etu.univ-amu.fr>
2024-02-03 21:27:02 +0100

    Changes to be committed: 	modified:   Predections.txt 	modified:   Test.py 	deleted:    TestAyoub.py 	modified:   __pycache__/Image.cpython-310.pyc 	modified:   __pycache__/architecture.cpython-310.pyc 	modified:   architecture.py

--- Test.py
+++ Test.py
@@ -24,13 +24,11 @@ choice = int(input('Type de representation: \n 0 pour GC et 1 pour HC '))
 Representations =["GC","HC"]
 print("Loading Traning Data ...")
 
-data = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
+train = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
 
+print("Loading Testing Data ...") 
 
-data = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
-
-
-data = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
+test = architecture.load_transform_test_dataset("./testimage/",Representations[choice]) 
 
 algo_bayes = { 'algo': 'multinomial naive bayes', 'force_alpha': True }
 algo_tree = { 'algo': 'decision tree', 'max_depth': 5, 'min_samples_split': 3 } 
@@ -41,6 +39,7 @@ choice_algo = int(input("Type d'algo : \n 0 pour multinomial naive bayes et 1 po
 algo_list = [algo_bayes,algo_tree]
 k = int(input('Number of splits : \n '))
 print('Training model ...')
+model = architecture.learn_model_from_dataset(train,algo_tree)
 print('Getting Predictions ...')
 print('Score :')
 print(architecture.estimate_model_score(data,algo_list[choice_algo],k))
