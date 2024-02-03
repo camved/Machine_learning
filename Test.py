ab38c1579d32a6387737b2841676fcf3f97b376d
ayoub <ayoub.barich@etu.univ-amu.fr>
2024-02-03 23:32:53 +0100

    On branch Ayoub Your branch is up to date with 'origin/Ayoub'.
    Changes to be committed:
    	modified:   Test.py
    	modified:   architecture.py
    
    Changes not staged for commit:
    	modified:   __pycache__/architecture.cpython-310.pyc

--- Test.py
+++ Test.py
@@ -20,23 +20,19 @@ def test_representation():
 
 
 #######TestGlobal#######
-choice = int(input('Type de representation: \n 0 pour GC et 1 pour HC'))
+choice = int(input('Type de representation: \n 0 pour GC et 1 pour HC '))
 Representations =["GC","HC"]
 print("Loading Traning Data ...")
 
-train = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
-
-print("Loading Testing Data ...") 
-
-test = architecture.load_transform_test_dataset("./testimage/",Representations[choice]) 
+data = architecture.load_transform_label_train_dataset("./data/Data/",Representations[choice]) 
 
 algo_bayes = { 'algo': 'multinomial naive bayes', 'force_alpha': True }
 algo_tree = { 'algo': 'decision tree', 'max_depth': 5, 'min_samples_split': 3 } 
-choice_algo = int(input("Type d'algo : \n 0 pour multinomial naive bayes et 1 pour decision tree"))
+choice_algo = int(input("Type d'algo : \n 0 pour multinomial naive bayes et 1 pour decision tree "))
 algo_list = [algo_bayes,algo_tree]
+k = int(input('Number of splits : \n '))
 print('Training model ...')
-model = architecture.learn_model_from_dataset(train,algo_list[choice_algo])
 print('Getting Predictions ...')
-predections = architecture.predict_sample_label(test,model[0])
-architecture.write_predictions(".","Predections.txt",predections,algo_list[choice_algo])
+print('Score :')
+print(architecture.estimate_model_score(data,algo_list[choice_algo],k))
 
